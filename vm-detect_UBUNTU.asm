1836b0b3a8d138f3bbd593285188fc1cc4982f50
kd992102 <30813510+kd992102@users.noreply.github.com>
2019-08-02 21:56:07 +0800

    Update vm-detect_UBUNTU.asm

--- vm-detect_UBUNTU.asm
+++ vm-detect_UBUNTU.asm
@@ -14,24 +14,20 @@ section .data
     MessageTwoL   EQU $-Messagetwo
 
 section .bss
-align 8
+    align 8
     StandardHandle resd 1
     Written        resd 1
 
 section .text
-Start:
-Start:
- sub   RSP, 8                                   ; Align the stack to a multiple of 16 bytes
 
- sub   RSP, 32                                  ; 32 bytes of shadow space
+Start:
+ sub   RSP, 8                                   
+ sub   RSP, 32                                  
  mov   ECX, STD_OUTPUT_HANDLE
  call  GetStdHandle
  mov   qword [REL StandardHandle], RAX
- add   RSP, 32                                  ; Remove the 32 bytes
-
- sub   RSP, 32 + 8 + 8                          ; Shadow space + 5th parameter + align stack
-                                                ; to a multiple of 16 bytes
-
+ add   RSP, 32                                  
+ sub   RSP, 32 + 8 + 8                          
  xor   RAX, RAX
  inc   RAX
  cpuid
@@ -42,24 +38,24 @@ Start:
 
 
 NOTVM:
- mov   RCX, qword [REL StandardHandle]          ; 1st parameter
- lea   RDX, [REL Message]                       ; 2nd parameter
- mov   R8, MessageLength                        ; 3rd parameter
- lea   R9, [REL Written]                        ; 4th parameter
- mov   qword [RSP + 4 * 8], NULL                ; 5th parameter
- call  WriteFile                                ; Output can be redirect to a file using >
- add   RSP, 48                                  ; Remove the 48 bytes
-
+ mov   RCX, qword [REL StandardHandle]          
+ lea   RDX, [REL Message]                       
+ mov   R8, MessageLength                        
+ lea   R9, [REL Written]                        
+ mov   qword [RSP + 4 * 8], NULL                
+ call  WriteFile                                
+ add   RSP, 48                                  
  xor   ECX, ECX
  call  ExitProcess
+ 
+ 
 VM:
-mov   RCX, qword [REL StandardHandle]          ; 1st parameter
- lea   RDX, [REL Messagetwo]                       ; 2nd parameter
- mov   R8, MessageTwoL                        ; 3rd parameter
- lea   R9, [REL Written]                        ; 4th parameter
- mov   qword [RSP + 4 * 8], NULL                ; 5th parameter
- call  WriteFile                                ; Output can be redirect to a file using >
- add   RSP, 48                                  ; Remove the 48 bytes
-
+ mov   RCX, qword [REL StandardHandle]          
+ lea   RDX, [REL Messagetwo]                   
+ mov   R8, MessageTwoL                        
+ lea   R9, [REL Written]                        
+ mov   qword [RSP + 4 * 8], NULL                
+ call  WriteFile                                
+ add   RSP, 48                                  
  xor   ECX, ECX
  call  ExitProcess
